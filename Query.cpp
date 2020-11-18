623b266d5ae3beb12167c280a59495c44dc51d77
Avital Pikovsky <avitali1996@gmail.com>
2020-11-18 15:31:12 +0200

    finish AdjacentQuery function

--- Query.cpp
+++ Query.cpp
@@ -7,7 +7,7 @@
 #include <cstddef>
 #include <iterator>
 #include <stdexcept>
-#include <regex>
+#include <string>
 using namespace std;
 /////////////////////////////////////////////////////////
 shared_ptr<QueryBase> QueryBase::factory(const string& s)
@@ -33,7 +33,7 @@ shared_ptr<QueryBase> QueryBase::factory(const string& s)
     return std::shared_ptr<QueryBase>(new AdjacentQuery(ans[1], ans[2]));
 
   else
-    throw invalid_argument("Unrecognized search");
+    throw invalid_argument("Unrecognized search\n");
 }
 /////////////////////////////////////////////////////////
 QueryResult AndQuery::eval (const TextQuery& text) const
@@ -59,44 +59,41 @@ QueryResult OrQuery::eval(const TextQuery &text) const
 QueryResult AdjacentQuery::eval (const TextQuery& text) const
 {
 
-// QueryResult left_result = text.query(left_query);
-    // QueryResult right_result = text.query(right_query);
-    // auto r_lines = make_shared<set<line_no>>();
-    // auto l_lines = make_shared<set<line_no>>();
-    // auto ret_lines = make_shared<set<line_no>>();
+  QueryResult left_result = text.query(left_query);
+  QueryResult right_result = text.query(right_query);
+  auto ret_lines = make_shared<set<line_no>>();
 
-    // r_lines->insert(right_result.begin(), right_result.end());
-    // l_lines->insert(left_result.begin(), left_result.end());
-
-    // int sum = 0;
-
-    // auto l = l_lines->begin();
-    // auto r = r_lines->begin();
-
-    // while(l != l_lines->end() || r != r_lines->end()){
-    //   if(abs(*l-*r)){
-    //     sum++;
-    //     ret_lines->insert(*l);
-    //     ret_lines->insert(*r);
-    //     ++l;
-    //     ++r;
-    //   }
-
-
-    // }
-    QueryResult left_result = text.query(left_query);
-    QueryResult right_result = text.query(right_query);
-    auto ret_lines = make_shared<set<line_no>>();
-    set_intersection(left_result.begin(), left_result.end(),
-                     right_result.begin(), right_result.end(),
-                     inserter(*ret_lines, ret_lines->begin()));
-
-    return QueryResult(rep(), ret_lines, left_result.get_file());
+  for(auto i = left_result.begin(); i != left_result.end(); i++){
+    for(auto j = right_result.begin(); j != right_result.end(); j++){
+      if(*i-*j == 1 || *j-*i == 1){
+        ret_lines->insert(*j);
+        ret_lines->insert(*i);        
+      }
+    }
+  }
+  return QueryResult(rep(), ret_lines, left_result.get_file());
 }
+
 /////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////
+
 std::ostream &print(std::ostream &os, const QueryResult &qr)
 {
+  if(qr.sought.find("AD") != string::npos){
+    os << "\"" << qr.sought << "\"" << " occurs " << 
+        qr.lines->size()/2 << " times:" <<std::endl;
+        
+        int count = 0;
+    for (auto num : *qr.lines)
+    {
+        os << "\t(line " << num + 1 << ") " 
+            << *(qr.file->begin() + num) << std::endl;
+        count++;
+        if((count) % 2 == 0 && count != qr.lines->size())
+          os << "\n";
+    }
+    return os;
+  }
     os << "\"" << qr.sought << "\"" << " occurs " << 
         qr.lines->size() << " times:" <<std::endl;
     for (auto num : *qr.lines)
